6891a7df6a1feaab3009384ff4f175bf1e6f7376
muddymaestro <medinho852@gmail.com>
2020-09-17 17:20:00 +0300

    change links format

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,7 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = 'https://redrosse.com'
+window.axios.defaults.baseURL = 'https://redrosse.com/public'
 //window.axios.defaults.baseURL = document.head.querySelector('meta[name="axios-base-url"]').content;
 
 /**
